e46599d55ebc38221077e221c172f3e511cf8ff0
Mina Mina <maouche_yamina@hotmail.com>
2022-02-23 16:09:40 +0100

    baabar est le plus cool

--- fonction.py
+++ fonction.py
@@ -14,6 +14,22 @@ def age():
     print("vous aurez 100 ans en: ", age_hundred)
 
 def volume():
-    r=input("le rayon:")
-    h=input("la hauteur: ")
-    volume= 1/3*m.pi() * float(r^2)  * float(h)
+    r=float(input("le rayon:"))
+    h=float(input("la hauteur: "))
+    volume= 1/3*(m.pi)*(r**2)*(h)
+    print("le volume est ", volume)
+
+def paire():
+    n=int(input("un chiffre :"))
+    if(n%2==0):
+        print("Nombre paire")
+    else: print("Nombre impaire")
+
+def fibonacci():
+    n = int(input("Entrez un nombre:"))
+    def fib(n): 
+        if(n <= 1):
+            return n
+        else:
+            return (fib(n-1) + fib(n-2))
+    print(fib(n))        
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from fonction import *
 
-volume()
+fibonacci()
